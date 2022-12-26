67f0b4c0225c4a1316fa06231f0fbb0d9150e7bb
Luke-Wojciechowski <grabarzpolski@gmail.com>
2022-12-26 22:06:13 +0100

    audio service separeted from ecs

---
+++ Assets/Source/Scripts/ECS/Audio/AudioService.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Pool;
+using Zenject;
+
+namespace Ingame.Audio
+{
+
+    public interface IAudio2DService
+    {
+        public void Play(string type, string name, bool allowRepetition = false);
+        public void Stop(string type, string name);
+        public void Pause(string type, string name);
+        public void Resume(string type, string name);
+    }
+
+    public interface IAudio3DService
+    {
+        public void Play3D(string type, string name, Transform transform, bool allowRepetition = true); 
+        public void Stop3D(string type, string name, Transform transform);
+        public void Pause3D(string type, string name, Transform transform);
+        public void Resume3D(string type, string name, Transform transform);
+    }
+
+    public interface IAudioService  : IAudio3DService,IAudio2DService
+    {
+        public void StopAll();
+        public void PauseAll();
+        public void ResumeAll();
+        
+        public static void ChangeVolume(float f)
+        {
+            AudioListener.volume = f;
+        }
+    }
+    public class AudioService : MonoBehaviour, IAudioService, IInitializable
+    {
+
+        [SerializeField] 
+        private AudioContainer audioContainer;
+
+        private Dictionary<string, Dictionary<string, (AudioClip, AudioWrapperSettings)>> _storedAudios;
+        private ObjectPool<AudioSource> _pool;
+        
+        private Dictionary<(string, string, GameObject), Queue<AudioSource>> _played3dAudioSources = new();
+        private Dictionary<(string, string), Queue<AudioSource>> _played2dAudioSources  = new();
+        
+        private Dictionary<(string, string, GameObject), Queue<AudioSource>> _paused3dAudioSources = new();
+        private Dictionary<(string, string), Queue<AudioSource>> _paused2dAudioSources = new();
+        
+        public void Initialize(){}
+        public void Awake()
+        {
+            Dictionary<string, Dictionary<string, (AudioClip,AudioWrapperSettings)>> dict = new ();
+            foreach (var typeWrapper in audioContainer.Audios)
+            {
+                var audioClips = typeWrapper.AudioWrappers.ToDictionary(e=> e.Name, e=>( e.AudioClip, e.AudioSettings));
+                dict.Add(typeWrapper.Name,audioClips);
+            }
+            _storedAudios = dict;
+            
+            _pool = new ObjectPool<AudioSource>(
+                OnAudioSourceCreate,
+                OnAudioSourceGet,
+                OnAudioSourceRelease,
+                OnAudioSourceDestroy
+            );
+        }
+        
+        private void FixedUpdate()
+        {
+            RemoveStoppedAudioSources(ref _played3dAudioSources);
+            RemoveStoppedAudioSources(ref _played2dAudioSources);
+        }
+
+        private void RemoveStoppedAudioSources<T>(ref Dictionary<T, Queue<AudioSource>> audioSources)
+        {
+            if(audioSources.Count<=0)
+                return;
+            
+            List<T> ids = audioSources.Keys.ToList();
+            foreach (var idKey in ids)
+            {
+                Queue<AudioSource> newAudios = new();
+                foreach (var audio in audioSources[idKey])
+                {
+                    if (audio.isPlaying)
+                    {
+                        newAudios.Enqueue(audio);
+                        continue;
+                    }
+                    ReleaseOnClipFinish(audio);
+                }
+
+                audioSources[idKey] = newAudios;
+            }
+        }
+        private AudioSource OnAudioSourceCreate()
+        {
+            var go = new GameObject("Audio Source");
+            var audioSource = go.AddComponent<AudioSource>();
+            return audioSource;
+        }
+
+        private void OnAudioSourceGet(AudioSource source)
+        {
+            //NOTHING
+        }
+
+        private void OnAudioSourceRelease(AudioSource source)
+        {
+            source.transform.parent = this.transform;
+        }
+		
+        private void OnAudioSourceDestroy(AudioSource source)
+        {
+            //NOTHING
+        }
+
+        private void ReleaseOnClipFinish(AudioSource source)
+        {
+            source.Stop();
+            source.clip = null;
+            source.transform.parent = this.transform;
+            
+            _pool.Release(source);
+        }
+        
+        private AudioSource GetAndSetupAudioSource(string type, string name,Transform parent)
+        {
+            var audioOption = _storedAudios[type][name];
+            var audioSettings = audioOption.Item2;
+
+            var audio = _pool.Get();
+            audio.clip = audioOption.Item1;
+            audio.priority = audioSettings.Priority;
+            audio.volume = audioSettings.Volume;
+            audio.pitch = audioSettings.Pitch;
+            audio.loop = audioSettings.Loop;
+            audio.spatialBlend = audioSettings.SpatialBlend;
+            audio.dopplerLevel = audioSettings.DopplerLevel;
+            audio.spread = audioSettings.Spread;
+            audio.minDistance = audioSettings.MinDistance;
+            audio.maxDistance = audioSettings.MaxDistance;
+            audio.transform.parent = parent;
+            audio.transform.localPosition = Vector3.zero;
+            
+            return audio;
+        }
+        
+        public void Play3D(string type, string name, Transform parent, bool allowRepetition = true)
+        {
+            
+            if (!allowRepetition 
+                && _played3dAudioSources.ContainsKey((type,name, parent.gameObject)) 
+                && _played3dAudioSources[(type,name, parent.gameObject)].Count >0)
+                return;
+            
+            var audio = GetAndSetupAudioSource(type,name,parent);
+            audio.Play();
+
+            if (!_played3dAudioSources.ContainsKey((type, name, parent.gameObject)))
+            {
+                _played3dAudioSources.Add((type, name, parent.gameObject),new Queue<AudioSource>()); 
+            }
+            
+            _played3dAudioSources[(type,name,gameObject)].Enqueue(audio);
+        }
+
+        public void Stop3D(string type, string name, Transform parent)
+        {
+            if (!_played3dAudioSources.ContainsKey((type,name, parent.gameObject)) 
+                || _played3dAudioSources[(type,name, parent.gameObject)].Count<=0)
+                return;
+            
+            var audio = _played3dAudioSources[(type, name, parent.gameObject)].Dequeue();
+            _pool.Release(audio);
+        }
+
+        public void Pause3D(string type, string name, Transform parent)
+        {
+            if (!_played3dAudioSources.ContainsKey((type,name, parent.gameObject))
+                || _played3dAudioSources[(type,name, parent.gameObject)].Count<=0)
+                return;
+            
+            var audio = _played3dAudioSources[(type, name, parent.gameObject)].Dequeue();
+            audio.Pause();
+
+            if (!_paused3dAudioSources.ContainsKey((type, name, parent.gameObject)))
+            {
+                _paused3dAudioSources.Add((type, name, parent.gameObject), new Queue<AudioSource>());
+            }
+            
+            _paused3dAudioSources[(type, name, parent.gameObject)].Enqueue(audio);
+
+        }
+
+        public void Resume3D(string type, string name, Transform parent)
+        {
+            if(!_paused3dAudioSources.ContainsKey((type,name, parent.gameObject))
+               || _paused3dAudioSources[(type,name, parent.gameObject)].Count<=0)
+                return;
+            
+            var audio = _paused3dAudioSources[(type, name, parent.gameObject)].Dequeue();
+            audio.Play();
+
+            _played3dAudioSources[(type, name, parent.gameObject)].Enqueue(audio);
+        }
+        
+        public void Play(string type, string name, bool allowRepetition = false)
+        {
+            
+            if (!allowRepetition 
+                && _played2dAudioSources.ContainsKey((type,name)) 
+                && _played2dAudioSources[(type,name)].Count >0)
+                return;
+            
+            var audio = GetAndSetupAudioSource(type,name, this.transform);
+            audio.Play();
+
+            if (!_played2dAudioSources.ContainsKey((type, name)))
+            {
+                _played2dAudioSources.Add((type, name),new Queue<AudioSource>()); 
+            }
+            
+            _played2dAudioSources[(type,name)].Enqueue(audio);
+        }
+
+        public void Stop(string type, string name)
+        {
+            if (!_played2dAudioSources.ContainsKey((type,name)) 
+                || _played2dAudioSources[(type,name)].Count<=0)
+                return;
+            
+            var audio = _played2dAudioSources[(type, name)].Dequeue();
+            _pool.Release(audio);
+        }
+
+        public void Pause(string type, string name)
+        {
+            if (!_played2dAudioSources.ContainsKey((type,name))
+                || _played2dAudioSources[(type,name)].Count<=0)
+                return;
+            
+            var audio = _played2dAudioSources[(type, name)].Dequeue();
+            audio.Pause();
+
+            if (!_paused2dAudioSources.ContainsKey((type, name)))
+            {
+                _paused2dAudioSources.Add((type, name), new Queue<AudioSource>());
+            }
+            
+            _paused2dAudioSources[(type, name)].Enqueue(audio);
+        }
+
+        public void Resume(string type, string name)
+        {
+            if(!_paused2dAudioSources.ContainsKey((type,name))
+               || _paused2dAudioSources[(type,name)].Count<=0)
+                return;
+            
+            var audio = _paused2dAudioSources[(type, name)].Dequeue();
+            audio.Play();
+
+            _played2dAudioSources[(type, name)].Enqueue(audio);
+        }
+        
+   
+        public void StopAll()
+        {
+            var keys3d = _paused3dAudioSources.Keys;
+            foreach (var key in keys3d)
+            {
+                Stop3D(key.Item1,key.Item2, key.Item3.transform);
+            }
+            
+            var keys2d = _paused2dAudioSources.Keys;
+            foreach (var key in keys2d)
+            {
+                Stop(key.Item1,key.Item2);
+            }
+            
+            keys3d = _played3dAudioSources.Keys;
+            foreach (var key in keys3d)
+            {
+                Stop3D(key.Item1,key.Item2, key.Item3.transform);
+            }
+            
+            keys2d = _played2dAudioSources.Keys;
+            foreach (var key in keys2d)
+            {
+                Stop(key.Item1,key.Item2);
+            }
+        }
+        public void PauseAll()
+        {
+            var keys3d = _played3dAudioSources.Keys;
+            foreach (var key in keys3d)
+            {
+                Pause3D(key.Item1,key.Item2, key.Item3.transform);
+            }
+            
+            var keys2d = _played2dAudioSources.Keys;
+            foreach (var key in keys2d)
+            {
+                Pause(key.Item1,key.Item2);
+            }
+        }
+
+        public void ResumeAll()
+        {
+            var keys3d = _paused3dAudioSources.Keys;
+            foreach (var key in keys3d)
+            {
+                Resume3D(key.Item1,key.Item2, key.Item3.transform);
+            }
+            
+            var keys2d = _paused2dAudioSources.Keys;
+            foreach (var key in keys2d)
+            {
+                Resume(key.Item1,key.Item2);
+            }
+        }
+    }
+}
