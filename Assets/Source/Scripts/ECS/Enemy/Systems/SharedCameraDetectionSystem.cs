b4cdf8544d09733e485267f6e4b65191049f8b6e
Luke-Wojciechowski <grabarzpolski@gmail.com>
2023-01-05 18:01:19 +0100

    section 9 decorated

--- Assets/Source/Scripts/ECS/Enemy/Systems/SharedCameraDetectionSystem.cs
+++ Assets/Source/Scripts/ECS/Enemy/Systems/SharedCameraDetectionSystem.cs
@@ -1,4 +1,5 @@
-﻿using Ingame.Enemy;
+﻿using System.Collections.Generic;
+using Ingame.Enemy;
 using Ingame.Movement;
 using Leopotam.Ecs;
 using UnityEngine;
@@ -12,9 +13,13 @@ namespace Ingame.Systems
         private readonly EcsFilter<EnemyStateModel,TransformModel,EnemyUseCameraRequest> _enemyFilter;
         //MUST BE only one shared camera!!!
         private readonly EcsFilter<CameraComponent,SharedCameraModel> _cameraFilter;
+     
+        private Dictionary<EcsEntity, Texture2D> _cashedTexturesForEnvironment = new ();
+        private Dictionary<EcsEntity, Texture2D> _cashedTexturesForAll = new ();
+        private Dictionary<EcsEntity, Texture2D> _cashedTexturesForPlayer = new ();
         
         private int _width=32, _height=32;
-        private int _pixelDetectionThreshold = 4;
+        private int _pixelDetectionThreshold = 1;
         [Range(0,1)]
         private float _percentageDetectionThreshold = 0.15f;
         public void Run()
@@ -32,27 +37,45 @@ namespace Ingame.Systems
             {
                 ref var model = ref _enemyFilter.Get1(enemy);
                 ref var transform = ref _enemyFilter.Get2(enemy);
+                ref var enemyEntity = ref _enemyFilter.GetEntity(enemy);
                 
                 //set camera position
                 camera.Camera.transform.parent = transform.transform;
                 camera.Camera.transform.localPosition = new Vector3(0, ENEMY_HEIGHT, 0);
                 camera.Camera.transform.LookAt(model.target);
-           
-                var environment = GetRenderTexture(camera.Camera,cameraModel.MaskForEnvironment);
-                var all = GetRenderTexture(camera.Camera,cameraModel.MaskForEnvironmentWithPlayer);
+                
+                if (!_cashedTexturesForEnvironment.ContainsKey(enemyEntity))
+                {
+                    var texture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
+                    _cashedTexturesForEnvironment.Add(enemyEntity, texture);
+                }
+                var environment = GetRenderTexture(camera.Camera,cameraModel.MaskForEnvironment, _cashedTexturesForEnvironment[enemyEntity]);
+                
+                if (!_cashedTexturesForAll.ContainsKey(enemyEntity))
+                {
+                    var texture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
+                    _cashedTexturesForAll.Add(enemyEntity, texture);
+                }
+                var all = GetRenderTexture(camera.Camera,cameraModel.MaskForEnvironmentWithPlayer,_cashedTexturesForAll[enemyEntity]);
             
                 //1st phase of player recognition
-                var visibilityOfPlayer = GetNumberOfPixelsOfPLayer(camera.Camera, environment, all);
+                var visibilityOfPlayer = GetNumberOfPixelsOfPLayer(environment, all);
                 model.visibleTargetPixels = visibilityOfPlayer;
                 if (visibilityOfPlayer>=_pixelDetectionThreshold)
                 {
                     model.isTargetDetected = true;
-                    _enemyFilter.GetEntity(enemy).Del<EnemyUseCameraRequest>();
+                    enemyEntity.Del<EnemyUseCameraRequest>();
                     continue;
                 }
                 
                 //2nd phase of player recognition
-                var player = GetRenderTexture(camera.Camera,cameraModel.MaskForPlayer);
+                if (!_cashedTexturesForPlayer.ContainsKey(enemyEntity))
+                {
+                    var texture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
+                    _cashedTexturesForPlayer.Add(enemyEntity, texture);
+                }
+                var player = GetRenderTexture(camera.Camera,cameraModel.MaskForPlayer,_cashedTexturesForPlayer[enemyEntity]);
+                
                 var percentageVision = GetPercentageVisibilityOfPixelsOfPLayer(camera.Camera, player, visibilityOfPlayer);
                 if (percentageVision >= _percentageDetectionThreshold)
                 {
@@ -62,7 +85,7 @@ namespace Ingame.Systems
             }
         }
 
-        private int GetNumberOfPixelsOfPLayer(Camera camera,Texture2D t1,Texture2D t2)
+        private int GetNumberOfPixelsOfPLayer(Texture2D t1,Texture2D t2)
         {
             var enviroPixels = t1.GetPixels();
             var allPixels = t2.GetPixels();
@@ -96,18 +119,16 @@ namespace Ingame.Systems
             }
             return numberOfVisiblePixels/totalNumberOfPlayersPixels;
         }
-        private Texture2D GetRenderTexture(Camera camera ,LayerMask mask)
+        private Texture2D GetRenderTexture(Camera camera ,LayerMask mask, Texture2D texture)
         {
             camera.cullingMask = mask;
             camera.gameObject.SetActive(true);
             camera.Render();
             RenderTexture.active = camera.targetTexture;
-            var texture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
             texture.ReadPixels(new(0,0,_width,_height),0,0);
             texture.Apply();
             camera.gameObject.SetActive(false);     
             return texture;
         }
-        
     }
 }
