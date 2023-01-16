7fb0f4b12f925405bf9552ed51eb2aa119a6f588
Vo1z <vitya.voody@gmail.com>
2023-01-17 00:33:20 +0100

    Path to the forest level was added

---
+++ Assets/Source/Scripts/ECS/LevelManagement/Hybrid/MoveToAnotherLevelTrigger.cs
@@ -0,0 +1,26 @@
+﻿using NaughtyAttributes;
+using Support;
+using UnityEngine;
+using Zenject;
+
+namespace Ingame.LevelManagement
+{
+	[RequireComponent(typeof(Collider))]
+	public sealed class MoveToAnotherLevelTrigger : MonoBehaviour
+	{
+		[SerializeField] [Scene] private int sceneToLoadIndex;
+		
+		private LevelManagementService _levelManagementService;
+		
+		[Inject]
+		private void Construct(LevelManagementService levelManagementService)
+		{
+			_levelManagementService = levelManagementService;
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			_levelManagementService.LoadLevel(sceneToLoadIndex);
+		}
+	}
+}
