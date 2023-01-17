5e438231424ce7ca48ed067365e2d407b5aa957a
Vo1z <vitya.voody@gmail.com>
2023-01-17 23:35:02 +0100

    Quests for town were implemented

---
+++ Assets/Source/Scripts/ECS/Quest/Core/Tags/ActiveQuestTag.cs
@@ -0,0 +1,4 @@
+﻿namespace Ingame.Quests
+{
+	public struct ActiveQuestTag { }
+}
---
+++ Assets/Source/Scripts/ECS/Quest/QuestSpecific/Components/OpenWithItemComponent.cs
@@ -0,0 +1,11 @@
+﻿using Ingame.QuestInventory;
+
+namespace Ingame.Quests.QuestSpecific
+{
+	public struct OpenWithItemComponent
+	{
+		public PickableItemConfig requiredItemToOpen;
+		public bool isOpenedOnce;
+		public string openAnimationTriggerName;
+	}
+}
