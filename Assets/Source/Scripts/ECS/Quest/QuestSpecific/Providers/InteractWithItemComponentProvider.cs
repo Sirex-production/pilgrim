076fa062482fa39fce6a2f2f98d76d4c325fb730
Vo1z <vitya.voody@gmail.com>
2023-01-18 22:17:03 +0100

    Quest implementation on forest level

---
+++ Assets/Source/Scripts/ECS/Quest/QuestSpecific/Providers/InteractWithItemComponentProvider.cs
@@ -0,0 +1,24 @@
+﻿using Ingame.QuestInventory;
+using NaughtyAttributes;
+using UnityEngine;
+using Voody.UniLeo;
+using Zenject;
+
+namespace Ingame.Quests.QuestSpecific
+{
+	public sealed class InteractWithItemComponentProvider : MonoProvider<InteractWithItemComponent>
+	{
+		[Required, SerializeField] private PerformActionWhenInteractedWithItem performActionWhenInteractedWithItem;
+		[Required, SerializeField] private PickableItemConfig requiredItem;
+
+		[Inject]
+		private void Construct()
+		{
+			value = new InteractWithItemComponent
+			{
+				performActionWhenInteractedWithItem = performActionWhenInteractedWithItem,
+				requiredItem = requiredItem
+			};
+		}
+	}
+}
