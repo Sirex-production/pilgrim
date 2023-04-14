efc0e44211475966bd05fe9f28e82754c1b015a2
Vo1z <vitya.voody@gmail.com>
2023-04-14 14:11:34 +0200

    Model preprocessor was added

---
+++ Assets/Source/Scripts/Postprocessors/Editor/ModelPostprocessor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Ingame.Postprocessors
+{
+	public sealed class ModelPostprocessor : AssetPostprocessor
+	{
+		private void OnPreprocessModel()
+		{
+			var modelImporter = assetImporter as ModelImporter;
+			if(modelImporter != null)
+				modelImporter.generateSecondaryUV = true;
+		}
+	}
+}
