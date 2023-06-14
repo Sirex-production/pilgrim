0347a3c0171af465504d76ea67c1fb8c92a09d76
CheeseCake <grabarzpolski@gmail.com>
2023-06-14 21:03:16 +0200

    train added

---
+++ Assets/Source/Scripts/ECS/Events/AppearTrain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DG.Tweening;
+using Ingame.Audio;
+using UnityEngine;
+using Zenject;
+
+namespace Ingame.Events
+{
+    public sealed class AppearTrain : MonoBehaviour
+    {
+        [SerializeReference] private Transform train;
+        [SerializeReference] private Transform destination;
+       
+        private AudioService _audioService;
+        private bool _wasTriggered;
+        [Inject] 
+        public void Construct(AudioService audioService)
+        {
+            _audioService = audioService;
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if(_wasTriggered)
+                return;
+            
+            if(!other.transform.CompareTag("Player"))
+                return;
+
+            _audioService.Play("town","train");
+            train.DOMove(destination.position, 28);
+            _wasTriggered = true;
+        }
+    }
+}
